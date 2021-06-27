3260d554a15f782e97dc328aa1236c44c86d895d
aayushkumar20 <37871733+aayushkumar20@users.noreply.github.com>
2021-06-27 20:21:34 +0530

    Create Packing and Unpacking with Python.py

---
+++ Packing and Unpacking with Python.py
@@ -0,0 +1,11 @@
+import socket 
+import struct
+host = "192.168.x.x" 
+port = *******your intrested port number******
+mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+mySocket.bind((host, port)) mySocket.listen(1)
+connection, address = mySocket.accept() 
+print("Connected to ", address) # struct pack method passes two parameters - format string and values
+message = struct.pack('hhl', 1, 2, 3) 
+connection.send(message)
+connection.close()
